8fca03c0b08eab56d0624b775340fa6783f7202e
SvetlanaZhak <lana.zhak@gmail.com>
2020-09-26 16:08:32 +0300

    add changes to the recommendations

--- src/components/Recomendation.js
+++ src/components/Recomendation.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
-import testimonial1 from "./testimonial1.jpg";
-import testimonial2 from "./testimonial2.jpg";
+import recommendation1 from "./recommendation1.JPG";
+import recommendation2 from "./recommendation2.jpg";
 export default class Recomendation extends Component {
   render() {
     return (
@@ -10,9 +10,9 @@ export default class Recomendation extends Component {
         <h1>
           <span>Recomendations</span>
         </h1>
-        <AliceCarousel autoPlay autoPlayInterval="3000">
-          <img src={testimonial1} alt="GoGocar" width="50%" />
-          <img src={testimonial2} alt="elisa" width="50%" />
+        <AliceCarousel>
+          <img src={recommendation1} alt="GoGocar" width="50%" />
+          <img src={recommendation2} alt="elisa" width="50%" />
         </AliceCarousel>
       </section>
     );
